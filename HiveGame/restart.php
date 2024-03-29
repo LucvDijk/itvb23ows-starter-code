86f882bbaae26ce2995543f8786ff6fc9aac1265
jajajaja234 <lu.van.dijk@st.hanze.nl>
2024-03-29 15:43:22 +0100

    Bug 5: Fix

--- HiveGame/restart.php
+++ HiveGame/restart.php
@@ -7,6 +7,9 @@ $_SESSION['hand'] = [0 => ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3], 1 
 $_SESSION['player'] = 0;
 
 $db = include_once 'database.php';
+$db->query('TRUNCATE TABLE moves');
+$db->query('TRUNCATE TABLE games');
+
 $db->prepare('INSERT INTO games VALUES ()')->execute();
 $_SESSION['game_id'] = $db->insert_id;
 
